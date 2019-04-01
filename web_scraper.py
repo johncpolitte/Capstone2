e01795891d86f371a6b9e4e66fc80f9b5b2dceac
John Politte <johncpolitte@yahoo.com>
2019-04-01 09:16:30 -0500

    add doc string to code

--- web_scraper.py
+++ web_scraper.py
@@ -26,23 +26,46 @@ def scrape(start_link):
     '''
     links = [start_link]
     print(links)
+    # Sets the article count at 0
     count = 0
+    # Links is the list of pages that will be appended to at the end of
+    # the for loop. The links page represents the website from the onion
+    # that has 20 links to articles on it. The html code from the more
+    # button (next-page) is appended to this list so that it will
+    # the links on the next page will also be scrapped
     for link in links:
         print(link)
+        # Gets the html code from the onion page with 20 article links on it
         soup = requests.get(link)
+        # Going to pull 10,000 articles. This if statement will end the
+        # function once the cound reaches 10,000
         if count < 10000:
+        # Turns html code from artilce page into beautiful soup object
             soup2 = BeautifulSoup(soup.text, 'lxml')
+        # Uses the 'h1' tag to find all of the links for the 20 articles
             linkers = soup2.find_all('h1', {'class': 'headline entry-title'})
+        # Initialize a list of all of the web address's for the 20 articles
+        # also empties the list when the loop comes back around for the
+        # next 20 articles
             https = []
+        # For loop that iterates through the list of 20 links of articles,
+        # and adds the web address to the https list
             for idx, x in enumerate(linkers):
+        # Gets the web address for each artcle and appends it to the list
                 https.append(linkers[idx].a['href'])
-            for link in https:
+        # Four loop that goes through the list of 20 articles from the
+        # links web page that has 20 articles
+            for ar_link in https:
+        # Initialize dictionary to insert into mongo
                 onion = {}
                 '''
                 need to replace . in html with DOT
                 '''
-                no_dot = link.replace('.', 'DOT')
-                page = requests.get(link)
+        # replace '.' with DOT in webaddress so it can be key in dictionary
+                no_dot = ar_link.replace('.', 'DOT')
+        # Gets html for webpage with the article
+                page = requests.get(ar_link)
+        # Turns into beautiful soup object
                 soup = BeautifulSoup(page.text, 'lxml')
                 content = soup.find_all('p')
                 content = list(content)
